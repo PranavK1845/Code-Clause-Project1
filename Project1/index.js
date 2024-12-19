12b25d9ddb9f77b6e27f96cba32f27687d53528d
Pranav P. Kadam <pranavkadam320@gmail.com>
2024-12-19 23:05:45 +0530

    Some changes

--- Project1/index.js
+++ Project1/index.js
@@ -2,10 +2,9 @@ window.addEventListener("load", () => {
     const loadingScreen = document.querySelector(".loading-screen");
     const mainContent = document.querySelector(".container");
   
-    // Wait for the animation to complete
     setTimeout(() => {
-      loadingScreen.style.display = "none"; // Hide the loading screen
-      mainContent.style.display = "block"; // Show the main content
-    }, 3000); // Match the duration of the animation
+      loadingScreen.style.display = "none"; 
+      mainContent.style.display = "block"; 
+    }, 3000); 
   });
   
